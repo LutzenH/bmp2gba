d4c97c7035a682518b9f6caf9d164dcb70c8863e
LutzenH <46445220+LutzenH@users.noreply.github.com>
2022-09-02 11:01:30 +0200

    Update b2g_platform.c

--- src/b2g_platform.c
+++ src/b2g_platform.c
@@ -2,8 +2,8 @@
 
 #if defined(B2G_WIN32)
 #include "b2g_platform_win32.c"
-#endif
-
-#if defined(B2G_LINUX)
+#elif defined(B2G_LINUX)
 #error Linux is currently not implemented!
+#else
+#error The platform you are compiling for is currently not implemented!
 #endif
